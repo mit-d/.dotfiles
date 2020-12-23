a74591af310fec11a274242ed5b462e66462c3ff
derek <derek@sericore.com>
2020-12-23 11:17:11 -0700

    Added fish vars

---
+++ fish/.config/fish/functions/fish_default_mode_prompt.fish
@@ -0,0 +1,22 @@
+# Defined in /tmp/fish.0YiXDa/fish_default_mode_prompt.fish @ line 2
+function fish_default_mode_prompt --description 'Display the default mode for the prompt'
+	if test "$fish_key_bindings" = "fish_vi_key_bindings"
+        or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
+        switch $fish_bind_mode
+            case default
+#                set_color --bold --background red white
+#                echo '[N]'
+            case insert
+#                set_color --bold --background green white
+#                echo '[I]'
+            case replace_one
+#                set_color --bold --background green white
+#                echo '[R]'
+            case visual
+#                set_color --bold --background magenta white
+#                echo '[V]'
+        end
+        set_color normal
+#        echo -n ' '
+    end
+end
