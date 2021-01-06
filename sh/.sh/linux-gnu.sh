977117cd1607c1077fbb83a9bbb666be9afbbfe5
derek <derek@sericore.com>
2021-01-06 11:54:49 -0700

    updated sudo/doas alias

--- sh/.sh/linux-gnu.sh
+++ sh/.sh/linux-gnu.sh
@@ -1,13 +1,15 @@
 setxkbmap -option caps:ctrl_modifier
+
+# Bedrock
 alias deb="strat -r debian"
 alias gen="strat -r gentoo"
 alias alp="strat -r alpine"
 
 if command -v doas >/dev/null
 then
-    alias el='doas $0'
+    alias el='doas $SHELL'
 else if command -v sudo
-    alias el='sudo -E $0'
+    alias el='sudo -E $SHELL'
 fi
 
 alias ls='ls --color'
