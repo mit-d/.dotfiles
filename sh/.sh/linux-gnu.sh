fb02c795da094b61d502f589cfe89479c9fbbcd7
derek <derek@sericore.com>
2021-01-06 11:51:07 -0700

    add ls alias

--- sh/.sh/linux-gnu.sh
+++ sh/.sh/linux-gnu.sh
@@ -3,10 +3,12 @@ alias deb="strat -r debian"
 alias gen="strat -r gentoo"
 alias alp="strat -r alpine"
 
-if command -v doas
+if command -v doas >/dev/null
 then
     alias el='doas $0'
 else if command -v sudo
     alias el='sudo -E $0'
 fi
+
+alias ls='ls --color'
 
