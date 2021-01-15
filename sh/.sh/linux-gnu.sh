879627359d3b30d2886085339522fa3848b987bd
derek <derekmttn@gmail.com>
2021-01-15 14:58:06 -0700

    Add aliases for android dev

--- sh/.sh/linux-gnu.sh
+++ sh/.sh/linux-gnu.sh
@@ -8,9 +8,13 @@ alias alp="strat -r alpine"
 if command -v doas >/dev/null
 then
     alias el='doas $SHELL'
-else if command -v sudo
+else if command -v sudo >/dev/null
     alias el='sudo -E $SHELL'
 fi
 
 alias ls='ls --color'
 
+# Added for debian sid installs
+export PATH="$PATH:/home/derek/Android/Sdk/platform-tools"
+alias wireshark-usb='sudo modprobe usbmon && sudo setfacl -m u:derek:r /dev/usbmon* && wireshark'
+alias sus='systemctl suspend'
