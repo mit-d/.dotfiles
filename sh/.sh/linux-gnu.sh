8c184530c715a7143e7deb79450a09bcaee63b19
derek <derekmttn@gmail.com>
2021-07-19 10:44:15 -0600

    Add backup function

--- sh/.sh/linux-gnu.sh
+++ sh/.sh/linux-gnu.sh
@@ -1,16 +1,23 @@
 setxkbmap -option caps:ctrl_modifier
 
-# Bedrock
-alias deb="strat -r debian"
-alias gen="strat -r gentoo"
-alias alp="strat -r alpine"
-
 if command -v doas >/dev/null
 then
     alias el='doas $SHELL'
 else if command -v sudo >/dev/null
     alias el='sudo -E $SHELL'
 fi
+
+bak() {
+    mkdir -p .bak
+    FILE=".bak/$1.$(date --iso-8601).bak"
+    num=1;
+    while [ -e "$FILE" ]
+    do
+        FILE=".bak/$1.$(date --iso-8601).bak\ ($num)"
+        let "num=num+1"
+    done
+    rsync -av "$1" "$FILE"
+}
 
 alias ls='ls --color'
 
