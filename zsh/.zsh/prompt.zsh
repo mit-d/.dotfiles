de9d18ea2b70a8398a2725d3d7526a297887e01b
mit-d <derekmttn@gmail.com>
2025-09-22 19:52:01 -0600

    feat: various fixes

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -8,14 +8,23 @@ autoload -Uz vcs_info
 # Configure git branch formats in vcs_info
 zstyle ':vcs_info:git:*' formats '(%b)'
 zstyle ':vcs_info:git:*' actionformats '(%b|%a)'
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:git:*' check-for-changes true
+zstyle ':vcs_info:git:*' stagedstr '%F{green}●%F{blue}'
+zstyle ':vcs_info:git:*' unstagedstr '%F{red}●%F{blue}'
+zstyle ':vcs_info:git:*' formats '%u%c (%b)'
 
-precmd() { vcs_info; VCS_MSG="${vcs_info_msg_0_}" }
+precmd() {
+  vcs_info
+  VCS_MSG="${vcs_info_msg_0_}"
+}
 
 setopt PROMPT_SUBST
-PROMPT="%B%F{red}%#%b%f "
 
+# Environment variables to display in the prompt
 ENV_VARS=(
   "DB_NAME"
+  "VIRTUAL_ENV"
 )
 
 # Function to build prompt with environment variables
@@ -28,20 +37,22 @@ build_env_prompt() {
   done
   echo "$env_prompt"
 }
+
+# Build the main prompt
+PROMPT="%B%F{red}%#%b%f "
 PROMPT="$(build_env_prompt)${PROMPT}"
-
 PROMPT="%~ $PROMPT"
+
+# Conditional RPROMPT to show VCS info only if available
+RPROMPT='%F{blue}${VCS_MSG:+${VCS_MSG}}%f'
+
+# Export prompts
 export PROMPT
-
-
-# RPROMPT='%F{blue}${VCS_MSG}%f'
-RPROMPT='%F{blue}${VCS_MSG}%f'
 export RPROMPT
 
+# PS2 for multiline commands
 PS2=" >> "
 export PS2
 
-# /etc/doas.conf:
-# permit persist setenv { -ENV PROMPT=$DOAS_PROMPT SSH_AUTH_SOCK } :wheel
-# permit nopass keepenv setenv { PATH } root as root
+# DOAS prompt customization
 export DOAS_PROMPT="%/ %B%F{yellow}%#%b%f "
